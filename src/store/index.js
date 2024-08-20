9d1ab97ed19708b8e7c4d518e8b62bc5c79c232d
nusaibah1 <charifnusaibah01@gmail.com>
2024-08-20 15:59:32 +0200

    storeImports(1)

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,11 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+import { toast } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+import router from '@/router'
+// import { applyToken } from '@/service/.js'
+// import { useCookies } from 'vue3-cookies'
+// const { cookies } = useCookies()
 
 export default createStore({
   state: {
@@ -12,3 +19,6 @@ export default createStore({
   modules: {
   }
 })
+
+
+
