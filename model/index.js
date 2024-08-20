a2bb7a5f80df89b2a2db3938ea51211cd3bd2ba3
nusaibah1 <charifnusaibah01@gmail.com>
2024-08-20 11:14:25 +0200

    model2Objects

--- model/index.js
+++ model/index.js
@@ -1,1 +1,10 @@
-//
+import { Users } from "./Users.js";
+import { Products } from "./Products.js";
+
+const users = new Users()
+const products = new Products()
+
+export {
+    users,
+    products
+}
