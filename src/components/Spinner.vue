60d073477930f8d68e40eb733d6eae958b048417
nusaibah1 <charifnusaibah01@gmail.com>
2024-08-21 09:58:06 +0200

    storeSetup&updates

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,11 +1,30 @@
 <template>
-    <div class="d-flex justify-content-center">
-        <div class="spinner-border" role="status">
-        </div>
-    </div>
+    <div class="loader"></div> 
+
+
 </template>
 <script>
+
 export default {
     name: 'SpinnerComp'
 }
 </script>
+<style>
+.loader {
+  width: 60px;
+  aspect-ratio: 1;
+  background: linear-gradient(#dc1818 0 0) bottom/100% 0% no-repeat #ccc;
+  -webkit-mask:  
+    radial-gradient(circle at 60% 65%, #000 62%, #0000 65%) top left, 
+    radial-gradient(circle at 40% 65%, #000 62%, #0000 65%) top right, 
+    linear-gradient(to bottom left, #000 42%,#0000 43%) bottom left , 
+    linear-gradient(to bottom right,#000 42%,#0000 43%) bottom right;
+  -webkit-mask-size: 50% 50%;
+  -webkit-mask-repeat: no-repeat;
+  animation: l19 2s infinite linear;
+}
+@keyframes l19 {
+    90%,100% {background-size:100% 100%}
+}
+</style>
+
--- src/store/index.js
+++ src/store/index.js
@@ -3,18 +3,249 @@ import axios from 'axios'
 import { toast } from 'vue3-toastify'
 import 'vue3-toastify/dist/index.css'
 import router from '@/router'
-// import { applyToken } from '@/service/.js'
-// import { useCookies } from 'vue3-cookies'
-// const { cookies } = useCookies()
+import { applyToken } from '@/service/AuthenticatedUser.js'
+import { useCookies } from 'vue3-cookies'
+const { cookies } = useCookies()
 
+const apiURL = 'https://nodeeomp-sf1q.onrender.com/'
+
+// If the user reloads the page after logging in
+applyToken(cookies.get('LegitUser')?.token)
 export default createStore({
   state: {
+    users: null,
+    user: null,
+    products: null,
+    recentProducts: null,
+    product: null
   },
   getters: {
   },
   mutations: {
+    setUsers(state, value) {
+      state.users = value
+    },
+    setUser(state, value) {
+      state.user = value
+    },
+    setProducts(state, value) {
+      state.products = value
+    },
+    setRecentProducts(state, value) {
+      state.recentProducts = value
+    },
+    setProduct(state, value) {
+      state.product = value
+    },
+
   },
   actions: {
+    // ==== Fetch All Users ========
+    async fetchUsers(context) {
+      try {
+        const { results, msg } = await (await axios.get(`${apiURL}users`)).data
+        if (results) {
+          context.commit('setUsers', results)
+        } else {
+          toast.error(`${msg}`, {
+            autoClose: 3000
+          })
+        }
+      } catch (e) {
+        toast.error(`${e.message}`, {
+          autoClose: 3000
+       
+        })
+      }
+    },
+        // ==== Fetch A User ========
+    async fetchUser(context, id) {
+      try {
+        const { result, msg } = await (await axios.get(`${apiURL}users/${id}`)).data
+        if (result) {
+          context.commit('setUser', result)
+        } else {
+          toast.error(`${msg}`, {
+            autoClose: 3000
+          
+          })
+        }
+      } catch (e) {
+        toast.error(`${e.message}`, {
+          autoClose: 3000
+         
+        })
+      }
+    },
+        // ==== Add a User ========
+    async register(context, payload) {
+      try {
+        const { msg, err, token } = await (await axios.post(`${apiURL}users/register`, payload)).data
+        if (token) {
+          context.dispatch('fetchUsers')
+          toast.success(`${msg}`, {
+            autoClose: 3000
+        
+          })
+          router.push({ name: 'login' })
+        } else {
+          toast.error(`${err}`, {
+            autoClose: 3000
+          
+          })
+        }
+      } catch (e) {
+        toast.error(`${e.message}`, {
+          autoClose: 3000
+      
+        })
+      }
+    },
+        // ==== Update a User ========
+    async updateUser(context, payload) {
+      try {
+        const { msg, err } = await (await axios.patch(`${apiURL}users/${payload.userID}`, payload)).data
+        if (msg) {
+          context.dispatch('fetchUsers')
+        } else {
+          toast.error(`${err}`, {
+            autoClose: 3000
+     
+          })
+        }
+      } catch (e) {
+        toast.error(`${e.message}`, {
+          autoClose: 3000
+       
+        })
+      }
+    },
+        // ==== Remove a User ========
+    async deleteUser(context, id) {
+      try {
+        const { msg, err } = await (await axios.delete(`${apiURL}users/${id}`)).data
+        if (msg) {
+          context.dispatch('fetchUsers')
+        } else {
+          toast.error(`${err}`, {
+            autoClose: 3000
+           
+          })
+        }
+      } catch (e) {
+        toast.error(`${e.message}`, {
+          autoClose: 3000
+        })
+      }
+    },
+
+ // ==== Fetch All Products=====
+    async fetchProducts(context) {
+      try {
+        const { results } = await (await axios.get(`${apiURL}products`)).data
+        if (results) {
+          context.commit('setProducts', results)
+        } else {
+          router.push({ name: 'login' })
+        }
+      } catch (e) {
+        toast.error(`${e.message}`, {
+          autoClose: 3000
+          
+        })
+      }
+
+    },
+        // ==== Fetch Recent Products ========
+    async recentProducts(context) {
+      try {
+        const { results, msg } = await (await axios.get(`${apiURL}products/recent`)).data
+        if (results) {
+          context.commit('setRecentProducts', results)
+        } else {
+          toast.error(`${msg}`, {
+            autoClose: 3000
+          })
+        }
+      } catch (e) {
+        toast.error(`${e.message}`, {
+          autoClose: 3000
+        })
+      }
+    },
+           // ==== Fetch A Product ========
+    async fetchProduct(context, id) {
+      try {
+        const { result, msg } = await (await axios.get(`${apiURL}products/${id}`)).data
+        if (result) {
+          context.commit('setProduct', result)
+        } else {
+          toast.error(`${msg}`, {
+            autoClose: 3000
+          
+          })
+        }
+      } catch (e) {
+        toast.error(`${e.message}`, {
+          autoClose: 3000
+        
+        })
+      }
+    },
+           // ==== Add a Product========
+    async addAProduct(context, payload) {
+      try {
+        const { msg } = await (await axios.post(`${apiURL}products/add`, payload)).data
+        if (msg) {
+          context.dispatch('fetchProducts')
+          toast.success(`${msg}`, {
+            autoClose: 3000
+          })
+        }
+      } catch (e) {
+        toast.error(`${e.message}`, {
+          autoClose: 3000
+      
+        })
+      }
+    },
+           // ==== Update a Product ========
+    async updateProduct(context, payload) {
+      try {
+        const { msg } = await (await axios.patch(`${apiURL}products/${payload.productID}`, payload)).data
+        if (msg) {
+          context.dispatch('fetchProducts')
+          toast.success(`${msg}`, {
+            autoClose: 3000
+         
+          })
+        }
+      } catch (e) {
+        toast.error(`${e.message}`, {
+          autoClose: 3000
+      
+        })
+      }
+    },
+           // ==== Remove a  Product ========
+    async deleteProduct(context, id) {
+      try {
+        const { msg } = await (await axios.delete(`${apiURL}products/${id}`)).data
+        if (msg) {
+          context.dispatch('fetchProducts')
+          toast.success(`${msg}`, {
+            autoClose: 3000
+ 
+          })
+        }
+      } catch (e) {
+        toast.error(`${e.message}`, {
+          autoClose: 3000
+        
+        })
+      }
+    }
+
   },
   modules: {
   }
